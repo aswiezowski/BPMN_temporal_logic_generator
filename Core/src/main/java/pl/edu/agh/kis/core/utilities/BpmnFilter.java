516b78b838dd49b1d492820d9196616ea8538475
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2015-11-12 18:49:07 +0100

    Change package name fix #2

--- Core/src/main/java/pl/edu/agh/kis/core/utilities/BpmnFilter.java
+++ Core/src/main/java/pl/edu/agh/kis/core/utilities/BpmnFilter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pl.edu.agh.kis.core.resources;
+package pl.edu.agh.kis.core.utilities;
 
 import java.io.File;
 import javax.swing.filechooser.FileFilter;
