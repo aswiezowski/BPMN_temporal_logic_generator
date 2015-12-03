591259bb67846c1842f0118f5c7605e374023be9
admin <admin@10.132.15.20>
2015-12-03 15:01:57 +0100

    Multiple merge detecting. Fix #17

--- Core/src/main/java/pl/edu/agh/kis/core/PatternExtractor.java
+++ Core/src/main/java/pl/edu/agh/kis/core/PatternExtractor.java
@@ -32,16 +32,16 @@ import java.util.List;
 import pl.edu.agh.kis.core.data.Node;
 import pl.edu.agh.kis.core.data.StructNode;
 import pl.edu.agh.kis.exceptions.BadPatternException;
+import pl.edu.agh.kis.patterns.Discriminator;
+import pl.edu.agh.kis.patterns.ExclusiveChoice;
 import pl.edu.agh.kis.patterns.ImplicitTermination;
 import pl.edu.agh.kis.patterns.MultipleChoice;
-import pl.edu.agh.kis.patterns.Pattern;
-import pl.edu.agh.kis.patterns.SynchronizingMerge;
+import pl.edu.agh.kis.patterns.MultipleMerge;
 import pl.edu.agh.kis.patterns.Pattern;
 import pl.edu.agh.kis.patterns.Sequence;
 import pl.edu.agh.kis.patterns.SimpleMerge;
+import pl.edu.agh.kis.patterns.Synchronization;
 import pl.edu.agh.kis.patterns.SynchronizingMerge;
-import pl.edu.agh.kis.patterns.Discriminator;
-import pl.edu.agh.kis.patterns.ExclusiveChoice;
 
 /**
  *
@@ -55,6 +55,7 @@ public class PatternExtractor {
         PATTERN_PRIORITY = new ArrayList<>();
         PATTERN_PRIORITY.add(new SynchronizingMerge());
         PATTERN_PRIORITY.add(new Discriminator());
+        PATTERN_PRIORITY.add(new MultipleMerge());
         PATTERN_PRIORITY.add(new ImplicitTermination());
         PATTERN_PRIORITY.add(new ExclusiveChoice());
         PATTERN_PRIORITY.add(new MultipleChoice());
---
+++ Core/src/main/java/pl/edu/agh/kis/patterns/MultipleMerge.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2015, pl.edu.agh
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package pl.edu.agh.kis.patterns;
+
+import edu.uci.ics.jung.algorithms.filters.FilterUtils;
+import edu.uci.ics.jung.graph.Graph;
+import java.rmi.UnexpectedException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import pl.edu.agh.kis.core.PatternExtractor;
+import pl.edu.agh.kis.core.data.AtomNode;
+import pl.edu.agh.kis.core.data.AtomNodeType;
+import pl.edu.agh.kis.core.data.Edge;
+import pl.edu.agh.kis.core.data.Node;
+import pl.edu.agh.kis.core.data.NodeType;
+import pl.edu.agh.kis.core.data.StructNode;
+import pl.edu.agh.kis.core.data.StructNodeType;
+import pl.edu.agh.kis.exceptions.BadPatternException;
+
+/**
+ *
+ * @author Jakub Piotrowski
+ */
+public class MultipleMerge extends Pattern {
+
+    @Override
+    public StructNode findPattern(Graph g, Node start) throws BadPatternException, UnexpectedException {
+        StructNode snode = null;
+        if (g.getOutEdges(start).size() == 2 && start.getType() == NodeType.ATOM && ((AtomNode)start).getAtomType() == AtomNodeType.TASK) {
+            Iterator<Edge> iter = g.getOutEdges(start).iterator();
+            List<Node> pathNodes1 = new ArrayList<>();
+            List<Node> pathNodes2 = new ArrayList<>();
+            AtomNode startPath1 = (AtomNode) iter.next().getEnd();
+            AtomNode startPath2 = (AtomNode) iter.next().getEnd();
+            AtomNode endPath1 = findEndPatternNode(g, startPath1, Arrays.asList(AtomNodeType.TASK), 0, pathNodes1);
+            AtomNode endPath2 = findEndPatternNode(g, startPath2, Arrays.asList(AtomNodeType.TASK), 0, pathNodes2);
+            if(endPath1 == null || endPath2 == null){
+                return null;
+            }
+            if (!endPath1.equals(endPath2)) {
+                throw new UnexpectedException("Bad end of pattern");
+            }
+            Graph subgraph1 = FilterUtils.createInducedSubgraph(pathNodes1, g);
+            Graph subgraph2 = FilterUtils.createInducedSubgraph(pathNodes2, g);
+            PatternExtractor pe = new PatternExtractor();
+            snode = new StructNode(start, endPath1, pe.extractPatterns(subgraph1, startPath1), pe.extractPatterns(subgraph2, startPath2), StructNodeType.MULTIPLE_MERGE);
+        }
+        return snode;
+    }
+
+}
